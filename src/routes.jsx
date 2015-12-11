6cfa900ae8d5f18de3081f752e2cdf767202160f
Chris Toomey <chris@ctoomey.com>
2015-12-11 08:52:07 -0500

    Flatten routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 
 import Layout from 'components/Layout';
-import Games from 'components/Games';
 import GameList from 'components/GameList';
 import Game from 'components/Game';
 import NewGame from 'components/NewGame';
 
 export default (
   <Route path='/' component={Layout}>
-    <Route path='/games' component={Games}>
-      <IndexRoute component={GameList} />
-      <Route path='new' component={NewGame} />
-      <Route path=':gameId/:player' component={Game} />
-    </Route>
+    <Route path='/games' component={GameList} />
+    <Route path='/games/new' component={NewGame} />
+    <Route path='/games/:gameId/:player' component={Game} />
   </Route>
 );
